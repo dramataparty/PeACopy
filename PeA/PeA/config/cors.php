254814aba316521b742b57db207e9743a4ce648e
dforrt <diogobailaoforte@gmail.com>
2025-12-16 17:03:07 +0000

    cors fixes

--- PeA/PeA/config/cors.php
+++ PeA/PeA/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', 'http://projetopea.eu', 'https://projetopea.eu'],
 
     'allowed_origins_patterns' => [],
 
